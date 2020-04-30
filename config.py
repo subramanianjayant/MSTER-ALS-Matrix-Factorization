fixed gradient sign on MSTER term

config.py
@@ -21,9 +21,9 @@ class MFConfig:
         self.A = np.mat(np.random.rand(self.n,self.d)) #initializations
         self.B = np.mat(np.random.rand(self.d,self.m))
 
-        self.lr = 0.01 #learning rate
-        self.lambda_ = 5 #regularization for MSTER term in loss
-        self.num_epochs = 200 #number of epochs for gradient descent
+        self.lr = 0.001 #learning rate
+        self.lambda_ = 10 #regularization for MSTER term in loss
+        self.num_epochs = 100 #number of epochs for gradient descent
 
     def dump(self):
         return self.M, self.A, self.B, self.k_A, self.k_B, self.lr, self.lambda_, self.num_epochs

optimize/functions.py
@@ -16,7 +16,7 @@ def MSTER(A, k):
     return ratio, vertices
 
 def loss(M, A, B, rA, rB, lambda_):
-    return np.linalg.norm(M-A*B, ord = 'fro')**2 - lambda_*(rA)#+rB)
+    return np.linalg.norm(M-A*B, ord = 'fro')**2 - lambda_*(rA+rB)
 
 def grad(A, vert):
     mat = np.mat(np.zeros((A.shape)))