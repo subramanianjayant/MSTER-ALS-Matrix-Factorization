2ed5abb59da1e9cc4be7016c00577e09dd1b0ef9
Jayant Subramanian <jayants@fb.com>
2020-09-22 23:44:52 -0400

    tSNE init commit

--- tsne/tsne.py
+++ tsne/tsne.py
@@ -1,6 +1,136 @@
 import math
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
+from sklearn import datasets, decomposition, metrics
+from matplotlib import pyplot as plt
 
-### Should run tsne
+############# CONSTANTS ##############
 
+DATA_SIZE = 1000
+ITERATIONS = 200
+
+######################################
+
+
+############## METHODS ###############
+
+def perplex_helper(Di, sigma):
+    Pi = np.exp(-1*Di.copy() * sigma)
+    Psum = np.sum(Pi)
+    perp_calc = np.log(Psum) + sigma * np.sum(Di * Pi) / Psum
+    Pf = Pi/Psum
+    return Pf, perp_calc
+
+
+def calc_p_vals(X, tol = 1e-5, perplexity = 30.0):
+    (n, d) = X.shape
+    pdists = metrics.pairwise.euclidean_distances(X, squared=True)
+    P = np.zeros((n, n))
+    sigmas = np.ones((n,1))
+
+    for i in range(n):
+        Di = pdists[i, np.concatenate((np.r_[0:i], np.r_[i+1:n]))]
+        Pf, perp_calc = perplex_helper(Di, sigmas[i])
+        #perplexity calculation - binary search for best param
+        min_ = -np.inf
+        max_ = np.inf
+        count = 0
+        diff = perp_calc - np.log(perplexity)
+        while diff > tol and count < 50:
+            if diff > 0:
+                min_ = sigmas[i].copy()
+                if max_ == np.inf or max_ == -np.inf:
+                    sigmas[i] = sigmas[i] * 2.
+                else:
+                    sigmas[i] = (sigmas[i] + max_) / 2.
+            else:
+                max_ = sigmas[i].copy()
+                if min_ == np.inf or min_ == -np.inf:
+                    sigmas[i] = sigmas[i] / 2.
+                else:
+                    sigmas[i] = (sigmas[i] + min_) / 2.
+            count+=1
+            Pf, perp_calc = perplex_helper(Di, sigmas[i])
+            diff = perp_calc - np.log(perplexity)
+
+        P[i, np.concatenate((np.r_[0:i], np.r_[i+1:n]))] = Pf
+    return P
+
+######################################################
+
+###################### MAIN #########################
+
+if __name__ == '__main__':
+    x_init, xlabels = datasets.make_blobs(n_samples=DATA_SIZE,n_features=100, centers=5)
+    y = np.random.rand(DATA_SIZE, 2)
+
+    #PCA and normalize into ball of radius 1
+    x = decomposition.PCA(n_components=30).fit_transform(x_init)
+    x = x/np.max(x)
+
+    (n,d) = x.shape
+
+    pvals = calc_p_vals(x)
+    pvals = pvals + pvals.T       # calculate symmetric pvals
+    pvals = pvals / np.sum(pvals)
+
+    #early exaggeration
+    pvals = pvals * 4.
+    pvals = np.maximum(pvals, 1e-12)
+
+    #gradient descent params
+    dy = np.zeros((n,2))
+    lr = 50
+
+    # params for van der maaten's momentum magic!
+    prev_y = np.zeros((n,2))
+    init_momentum = 0.5
+    st_momentum = 0.8
+    gains = np.ones((n, 2))
+    min_gain = 0.01
+    eta = 500
+
+
+    for iter in range(ITERATIONS):
+
+        #compute qvals -> computation trick from van der maaten's code
+        sum_Y = np.sum(np.square(y), 1)
+        num = -2. * np.dot(y, y.T)
+        num = 1. / (1. + np.add(np.add(num, sum_Y).T, sum_Y))
+        num[range(n), range(n)] = 0.
+        qvals = num / np.sum(num)
+        qvals = np.maximum(qvals, 1e-12)
+
+        #compute gradient -> computation trick again stolen from van der maaten
+        PQ = pvals - qvals
+        for i in range(n):
+            dy[i,:] = np.sum(np.tile(PQ[:, i] * num[:, i], (2, 1)).T * (y[i, :] - y), 0)
+
+        # van der maaten's momentum magic
+        # if iter < 20:
+        #     momentum = init_momentum
+        # else:
+        #     momentum = st_momentum
+        # gains = (gains + 0.2) * ((dy > 0.) != (iy > 0.)) + \
+        #         (gains * 0.8) * ((dy > 0.) == (iy > 0.))
+        # gains[gains < min_gain] = min_gain
+        # iy = momentum * iy - eta * (gains * dy)
+        # y = y + iy
+        # y = y - np.tile(np.mean(y, 0), (n, 1))
+
+        #our shitty gd implementation
+        y = y-lr*dy
+
+        #print loss value
+        if (iter + 1) % 10 == 0:
+            L = np.sum(pvals * np.log(pvals / qvals))  #KL divergence
+            print("Iteration %d: error is %f" % (iter + 1, L))
+
+        #stop early exaggeration
+        if iter == 100:
+            pvals = pvals/4
+
+    plt.scatter(y[:,0], y[:,1])
+    plt.show()
+
+#####################################################
